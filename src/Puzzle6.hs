f4063d346927c804f1c150e91176bbafeda166c3
Andreas Fröwis <andreas.froewis@gmail.com>
2021-01-18 22:21:27 +0100

    Implemented Puzzle 6 (Part 2)

--- src/Puzzle6.hs
+++ src/Puzzle6.hs
@@ -2,7 +2,7 @@ module Puzzle6 (puzzle6) where
 
 import Paths_aoc2020
 import Data.List.Split (splitOn)
-import Data.List (nub)
+import Data.List (nub, intersect)
 
 puzzle6 :: IO()
 puzzle6 = do
@@ -10,9 +10,13 @@ puzzle6 = do
        contents <-  readFile filePath
        let groups = splitOn "\n\n" contents
        let answersPerGroup = map lines groups
-       
+
        -- Part 1
        let uniqueAnswersPerGroup = map (nub . concat) answersPerGroup
-       let answer = foldl (\acc x -> acc + length x) 0 uniqueAnswersPerGroup
+       let score = foldl (\acc x -> acc + length x) 0 uniqueAnswersPerGroup
+       putStrLn $ "Number of answers: " ++ show score
 
-       putStrLn $ "Number of answers: " ++ show answer
+       -- Part 2
+       let sharedAnswers = map (nub . foldl1 intersect) answersPerGroup
+       putStrLn $ "Number of shared answers: " ++ show (foldl (\acc x -> acc + length x) 0 sharedAnswers)
+
