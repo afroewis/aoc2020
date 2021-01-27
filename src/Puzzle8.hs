f54454aeda5e36de9ef8600aac86844957c24a65
Andreas Fröwis <andreas.froewis@gmail.com>
2021-01-27 17:53:02 +0100

    Implemented Part 1 of Puzzle 8

--- src/Puzzle8.hs
+++ src/Puzzle8.hs
@@ -4,14 +4,30 @@ import Paths_aoc2020
 import Data.Text.Read
 import Data.Text (pack)
 
-data Instruction = Instruction {inst :: InstructionType, val :: Int}
+data Instruction = Instruction {instr :: InstructionType, val :: Int} deriving (Eq)
 instance Show Instruction where
   show (Instruction type' val) = show type' ++ " " ++ show val
 
-data InstructionType = Jmp | Acc | Nop deriving (Show)
+data ProgramState = ProgramState {state_instr :: Int, state_val :: Int}
+data InstructionType = Jmp | Acc | Nop deriving (Eq, Show)
 
---execute :: [Instruction] -> Int -> Int -> Int
---execute instrs i acc = 3
+execute :: [Instruction] -> [Int] -> Int -> Int -> Int
+execute instrs ranInstrs i acc = if length (filter (==i) ranInstrs') == 2
+                                 then acc
+                                 else execute instrs ranInstrs' (state_instr newState) (state_val newState)
+   where newState = executeInstruction currInstr currState
+         currState = ProgramState i acc
+         currInstr = instrs !! i
+         ranInstrs' = ranInstrs ++ [i]
+
+executeInstruction :: Instruction -> ProgramState -> ProgramState
+executeInstruction instruction state = ProgramState nextInstr nextVal
+  where nextInstr = if instr instruction == Jmp
+                    then state_instr state + val instruction
+                    else state_instr state + 1
+        nextVal = if instr instruction == Acc
+                            then state_val state + val instruction
+                            else state_val state
 
 parseLine :: String -> Instruction
 parseLine s = Instruction type' val
@@ -24,7 +40,6 @@ parseLine s = Instruction type' val
 
 readDecimal :: String -> Int
 readDecimal s = case reader of
-                 Left _ -> 0
                  Right r -> fst r
                 where reader = signed decimal $ pack s
 
@@ -35,4 +50,4 @@ puzzle8 = do
      let fileLines = lines contents
      let instructions = map parseLine fileLines
 
-     mapM_ print instructions
+     print $ execute instructions [] 0 0
