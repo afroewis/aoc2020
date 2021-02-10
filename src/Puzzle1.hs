f44106bb02506c6bd79991ee58167318fc069fa5
Andreas Fröwis <andreas.froewis@gmail.com>
2021-02-10 22:42:52 +0100

    Removed obsolete read function

--- src/Puzzle1.hs
+++ src/Puzzle1.hs
@@ -9,10 +9,7 @@ puzzle1 :: IO()
 puzzle1 = do
        filePath <- getDataFileName "data/puzzle1-input.txt"
        contents <- readFile filePath
-       let ints = map read' . words $ contents
+       let ints = map read . words $ contents
 
        print $ head [(x,y, x*y) | (x:ys) <- tails ints, y <- ys, x+y == 2020]
-       print $ head [(x,y,z, x*y*z) | x <- ints, y<- ints, z <- ints, x+y+z == 2020]
-
-read' :: String -> Int
-read' = read
+       print $ head [(x,y,z, x*y*z) | x <- ints, y <- ints, z <- ints, x+y+z == 2020]
